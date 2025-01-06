1f55517503e03f6f0c1594096f2a398f74cb8133
TheunissenSil <theunissensil@gmail.com>
2025-01-06 16:12:04 +0100

    Initalize Wordpress plugin

---
+++ Pittig-bakkie-feedback-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Pittig bakkie feedback plugin 
+ * Description: This plugin allows the clients of Pittig bakkie to give feedback on their website.
+ * Version: 1.0.0
+ * Author: Pittig bakkie
+ * Author URI: https://pittigbakkie.nl/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
